f4c7a155f71df1d2ba6a0a2d0de3129e04883744
han <zohegs@gmail.com>
2025-09-14 20:50:03 +0800

    feat: add background color to test window
    Set the background color of the test window to a specific orange hue (hsl(21, 100%, 54%)) for better visual distinction during testing.

--- dist/test/index.js
+++ dist/test/index.js
@@ -3,6 +3,7 @@ class Boot extends native.AbstractBoot {
     main() {
         this.run('./index.html', {
             'frame': false,
+            'background': 'hsl(21, 100%, 54%)'
         });
         // this.run('../../../clickgo/dist/test/desktop/index.html');
     }
--- dist/test/index.ts
+++ dist/test/index.ts
@@ -5,6 +5,7 @@ class Boot extends native.AbstractBoot {
     public main(): void {
         this.run('./index.html', {
             'frame': false,
+            'background': 'hsl(21, 100%, 54%)'
         });
         // this.run('../../../clickgo/dist/test/desktop/index.html');
     }
